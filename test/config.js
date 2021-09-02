25e98cd4d5eb8df63a65e1c7b2911721591d1e48
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-09-02 09:58:16 +0530

    add test config.js

---
+++ test/config.js
@@ -0,0 +1,22 @@
+// Testnet V3 config
+module.exports = {
+    rpc: {
+        parent: '',
+        child: '', // This is the MATIC testnet RPC
+    },
+    fx_portal: {
+        parent: {
+            erc20: '0xe9c7873f81c815d64c71c2233462cb175e4765b3',
+        },
+        child: {
+            erc20: '0xDDE69724AeFBdb084413719fE745aB66e3b055C7',
+        },
+    },
+    user1: {
+        "privateKey": "",
+        "address": ""
+    },
+    user2: {
+        address: '<paste address here>', // Your address
+    },
+}
