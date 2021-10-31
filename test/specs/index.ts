e91e34876601da698e4415b85ee18077af1c2479
ujjwalgupta94 <ujjwal@matic.network>
2021-10-31 13:05:31 +0530

    rm console node_env

--- test/specs/index.ts
+++ test/specs/index.ts
@@ -5,9 +5,6 @@ import { FxPortalPlugin } from "@maticnetwork/maticjs-fxportal";
 use(Web3ClientPlugin);
 use(FxPortalPlugin);
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
-
-
 import './erc20.spec'
 // import './erc721.spec'
 // import './pos_client.spec'
