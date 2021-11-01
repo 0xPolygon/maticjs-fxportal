a2e076480144de7ecc80e23317e0731342dac6b8
ujjwalgupta94 <ujjwal@matic.network>
2021-11-01 13:02:12 +0530

    publish version 1.0.0

--- build_helper/npm.export.js
+++ build_helper/npm.export.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./matic-fx-portal.node.js')
+  module.exports = require('./matic-fx-portal.node.min.js')
 } else {
   module.exports = require('./matic-fx-portal.node.js')
 }
