8850c5e7764febd7281d2dfd01a92d09c0fb06dd
ujjwalgupta94 <ujjwal@matic.network>
2021-11-30 10:51:33 +0530

    rm check for gasprice

--- test/specs/erc20.spec.ts
+++ test/specs/erc20.spec.ts
@@ -75,7 +75,7 @@ describe('ERC20', () => {
         expect(result).to.have.not.property('maxFeePerGas')
         expect(result).to.have.not.property('maxPriorityFeePerGas')
         // expect(result).to.have.property('gasPrice')
-        expect(result['gasPrice']).to.be.an('number').gt(0);
+        // expect(result['gasPrice']).to.be.an('number').gt(0);
         expect(result).to.have.property('chainId', 80001);
     });
 
